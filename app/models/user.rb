1255ba565a3ecfc1ce04a2bed0a1681219954b98
Toan Dang <19522357@gm.uit.edu.vn>
2021-12-12 17:53:59 +0700

    nghỉ đăng ký r hmu hmu

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #  devise :database_authenticatable, :registerable,
+  #        :rememberable, :validatable
+
          has_one :customer
          has_one :staff
 end
